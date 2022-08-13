7009213f8b37bd4e337a245f35caf2e9726768bb
TangledWiresYT <daniel.smith@grism.com>
2022-08-13 16:25:35 +0100

    a

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.use(express.static(path.join(__dirname, '.')))
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
+app.get('/credits', (req, res) => {
+    res.sendFile(`${__dirname}/credits.html`);
+});
+app.get('/settings', (req, res) => {
+    res.sendFile(`${__dirname}/settings.html`);
+});
 app.listen(3333, () => {
     console.log('Application listening on port 3333!');
 });
