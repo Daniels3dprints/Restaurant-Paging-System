b7731ff950fea21dbbcc403049ee3e7448ef294f
TangledWiresYT <68852080+TangledWiresYT@users.noreply.github.com>
2022-09-22 12:53:22 +0000

    a

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,6 +2,7 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
     resolve: {
+      modules: ['fs','net'],
       fallback: {
         "fs": require.resolve('fs'),
         "net": require.resolve('net'),
