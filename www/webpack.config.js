1545c65856c20afae2b917baddea5ca317aaaffc
TangledWiresYT <daniel.smith@grism.com>
2022-10-08 21:04:45 +0100

    a

---
+++ www/webpack.config.js
@@ -0,0 +1,25 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+    resolve: {
+      fallback: {
+        "fs": require.resolve('fs'),
+        "net": require.resolve('net'),
+      } 
+    },
+    entry: {
+      index: './src/index.js',
+      readConfig: './src/readConfig.js',
+      pagerController: './src/pagerController.js',
+      pagerTest: './src/pagerTest.js',
+      restartServer: './src/restartServer.js'
+    },
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/dist',
+    },
+    plugins: [
+      new NodePolyfillPlugin()
+    ]
+  
+  };
